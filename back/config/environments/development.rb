8a3392730ee830511569db4f50ecc3b1c3426f3d
malonetheressa <theressadmalone@gmail.com>
2024-06-02 19:04:24 +0200

    :alembic: test cors errors

--- back/config/environments/development.rb
+++ back/config/environments/development.rb
@@ -73,4 +73,10 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  Rails.application.configure do
+    # Allow connections to local server from 'localhost' and '127.0.0.1'
+    config.hosts << "localhost"
+    config.hosts << "127.0.0.1"
+  end
 end
