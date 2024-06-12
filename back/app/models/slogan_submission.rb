aa941f1c2b4a9059721593a07c082f6e933b4835
malonetheressa <theressadmalone@gmail.com>
2024-06-13 01:15:49 +0200

    :hammer: revert comment

--- back/app/models/slogan_submission.rb
+++ back/app/models/slogan_submission.rb
@@ -1,6 +1,6 @@
 class SloganSubmission < ApplicationRecord
     validates :first_name, presence: true
     validates :last_name, presence: true
-    validates :email_address, presence: true #format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :email_address, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
     validates :slogan_idea, presence: true, length: { minimum: 5 }
 end
